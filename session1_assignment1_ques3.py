71e54a3b279fc3fc4ba2cd0a593e528d9a8a06b2
vinodkarnatak29 <41187693+vinodkarnatak29@users.noreply.github.com>
2018-07-22 20:14:57 +0530

    Add files via upload

---
+++ session1_assignment1_ques3.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# #### Python program to accept the user's first and last name and then getting them printed in the the reverse order with a space between first name and last name.
+# 
+
+# In[3]:
+
+
+# @firstName variable for user first name.
+# @lastName variable for user last name.
+# @input() method ask the user for a string of data and simply returns the string
+
+firstName = input("Enter your First Name : ")
+lastName = input("Enter your Last Name : ")
+print (lastName + " " + firstName)
+
